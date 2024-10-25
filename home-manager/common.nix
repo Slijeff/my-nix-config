ba8e04c85bb2d6b66aade32708a377edd2fe3d05
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2024-10-25 13:25:32 -0500

    add eza

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -23,19 +23,8 @@
     git
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
   home.sessionVariables = {
     # EDITOR = "emacs";
@@ -48,7 +37,7 @@
     enable = true;
     shellAliases = {
       switch = "darwin-rebuild switch --flake ~/nix-config/";
-      ll = "ls -la";
+      ee = "eza";
     };
     enableCompletion = true;
     autosuggestion = {
@@ -74,5 +63,21 @@
     enable = true;
     userEmail = "31459252+Slijeff@users.noreply.github.com";
     userName = "Jeffrey Hui";
+    ignores = [
+      "*~"
+      ".DS_Store"
+    ];
+  };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    icons = "auto";
+    git = true;
+    extraOptions = [
+      "--all"
+      "--group-directories-first"
+      "--long"
+    ];
   };
 }
