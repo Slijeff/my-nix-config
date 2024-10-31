a19382f97fef64a716ce8a19febb43add053f859
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2024-10-31 18:48:20 -0500

    add modulation

--- home-manager/nixvim/nixvim.nix
+++ home-manager/nixvim/nixvim.nix
@@ -1,9 +1,21 @@
 {nixvim, ...}: {
   imports = [
     nixvim.homeManagerModules.nixvim
+    ./options.nix
   ];
   programs.nixvim = {
     enable = true;
+    defaultEditor = true;
+
+    performance = {
+      combinePlugins = {
+        enable = true;
+        standalonePlugins = [
+          "nvim-treesitter"
+        ];
+      };
+      byteCompileLua.enable = true;
+    };
 
     colorschemes = {
       catppuccin.enable = true;
