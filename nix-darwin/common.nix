b8122f93c59c7a7541b24d0fc2c0fd75dba61ea7
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2024-11-09 16:32:09 -0600

    fix optimise store error as prompted

--- nix-darwin/common.nix
+++ nix-darwin/common.nix
@@ -78,12 +78,12 @@
 
   nix = {
     settings = {
-      auto-optimise-store = true;
+      # auto-optimise-store = true;
       experimental-features = "nix-command flakes";
     };
     gc = {
       automatic = true;
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 3d";
     };
   };
 }
